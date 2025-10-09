0969d2b837c97e89b2b7a3777219c09553420293
Orel Vilchise <orel.vilchis@morsa.com.mx>
2025-10-09 09:45:51 -0500

    conexion

--- panel/config/global.php
+++ panel/config/global.php
@@ -1,8 +1,8 @@
 <?php 
 //Ip de la pc servidor de base de datos
 //define("DB_HOST","sql5c25c.carrierzone.com");
-//define("DB_HOST","srv467.hstgr.io");
-define("DB_HOST","localhost");
+define("DB_HOST","151.106.110.3");
+//define("DB_HOST","localhost");
 
 //Nombre de la base de datos
 
